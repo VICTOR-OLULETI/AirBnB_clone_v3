1b7a7ef004c0a812bbb15b9ce66ecbb822edd2c2
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-06 15:38:39 +0100

    gives the status of my API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" This script defines methods for the api """
 from flask import jsonify, request
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -9,3 +11,22 @@ def state():
     if request.method == 'GET':
         response = {"status": "OK"}
         return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ This function returns the number of objects of each table """
+    if request.method == 'GET':
+        my_dict = {}
+        temp_dict = {
+                "amenities": "Amenity",
+                "cities": "City",
+                "places": "Place",
+                "reviews": "Review",
+                "states": "State",
+                "users": "User"
+                }
+        for key, value in temp_dict.items():
+            my_dict[key] = storage.count(value)
+        resp = jsonify(my_dict)
+        return resp
