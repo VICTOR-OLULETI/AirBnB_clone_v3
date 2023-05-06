3ae2e459a3306b2a3234e0e52f18a803bc688621
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-06 15:17:50 +0100

    shows the status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
-
+# get the value of HBNB_API_HOST environment variable or use default 0.0.0.0
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+# get the value of HBNB_API_PORT' environment variable or use default 5000
+port = os.getenv('HBNB_API_PORT', 5000)
 @app.teardown_appcontext
 def cleanup(exception):
     """ Closes down the storage session """
@@ -16,4 +18,5 @@ def cleanup(exception):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    """ start Flask app """
+    app.run(host=host, port=port, threaded=True)
