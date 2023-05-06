1153f2195803637974d7c407029e43216ec35164
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-06 15:00:09 +0100

    returns status of the api

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from flask import jsonify, request
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'])
+def state():
+    """ This function returns the status of the api """
+    if request.method == 'GET':
+        response = {"status": "OK"}
+        return jsonify(response)
