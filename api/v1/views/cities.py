dcc57e45d6c611d0d9d2a502e9f36778dd9ce81e
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-10 08:32:10 +0100

    modified the cities.py to handle all REST api functions;

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,14 +6,19 @@ from models import storage
 from models.city import City
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
+@app_views.route(
+        '/states/<state_id>/cities', methods=['GET', 'POST'],
+        strict_slashes=False)
 def city_1(state_id=None):
     """This function retrieves all the list of state objects """
     resp = storage.all("City")
-    resp1 = [i.to_dict() for i in resp.values() if i.state_id == state_id]
     resp2 = storage.get("State", state_id)
+    resp1 = [i.to_dict() for i in resp2.cities]
+    # resp1 = [i.to_dict() for i in resp.values() if i.state_id == state_id]
     if request.method == 'GET':
         if resp1 is None:
+            abort(404, {'error': 'Not found'})
+        if resp2 is None:
             abort(404, {'error': 'Not found'})
         return jsonify(resp1)
 
@@ -24,10 +29,6 @@ def city_1(state_id=None):
         if not resp:
             """ if response is none """
             abort(400, {'error': 'Not a JSON'})
-
-        # if resp1 is None:
-        #    abort(404, {'error': 'Not found'})
-
         if resp.get('name') is None:
             abort(400, {'error': 'Missing name'})
         # State = classes.get('State')
@@ -36,24 +37,23 @@ def city_1(state_id=None):
         return make_response(jsonify(new_state.to_dict()), 201)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'])
+@app_views.route(
+        '/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'],
+        strict_slashes=False)
 def city_2(city_id=None):
     """ This function returns the status of the api """
     resp = storage.get('City', city_id)
     if request.method == 'GET':
-        # resp = storage.get(State, state_id)
         if resp is None:
             abort(404, 'Not found')
-
         return jsonify(resp.to_dict())
 
     if request.method == 'DELETE':
         if resp is None:
             abort(404, 'Not found')
         else:
-            storage.delete(resp)
+            resp.delete()
             storage.save()
-            del resp
         return make_response(jsonify({}), 200)
 
     if request.method == 'PUT':
@@ -64,8 +64,7 @@ def city_2(city_id=None):
             abort(400, 'Not a JSON')
 
         for key, value in data.items():
-            if key not in ['id', 'created_at', 'updated_at']:
+            if key not in ['id', 'state_id', 'created_at', 'updated_at']:
                 setattr(resp, key, value)
-        # obj = resp(**data)
         resp.save()
         return make_response(jsonify(resp.to_dict()), 200)
