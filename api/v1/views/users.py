9a501b058c86eb09e166b9a753f93f104f79695f
VICTOR-OLULETI <oluletiv@gmail.com>
2023-05-10 17:07:36 +0100

    modified users.py file to perform all REST API function

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,10 +6,9 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET', 'POST'])
-# @app_views.route('/users/', methods=['GET', 'POST'])
+@app_views.route('/users', methods=['GET', 'POST'], strict_slashes=False)
 def user_1():
-    """This function retrieves all the list of state objects """
+    """This function retrieves all the list of User objects """
     if request.method == 'GET':
         resp = storage.all("User")
         resp1 = [i.to_dict() for i in resp.values()]
@@ -21,42 +20,38 @@ def user_1():
             """ if response is none """
             abort(400, {'error': 'Not a JSON'})
 
-        if resp.get('name') is None:
-            abort(400, {'error': 'Missing name'})
+        if resp.get('email') is None:
+            abort(400, {'error': 'Missing email'})
+        if resp.get('password') is None:
+            abort(400, {'error': 'Missing password'})
         new_user = User(**resp)
         new_user.save()
         return make_response(jsonify(new_user.to_dict()), 201)
 
 
-@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
+@app_views.route(
+        '/users/<user_id>', methods=['GET', 'DELETE', 'PUT'],
+        strict_slashes=False)
 def user_2(user_id=None):
     """ This function returns the status of the api """
     resp = storage.get('User', user_id)
+    if resp is None:
+        abort(404, 'Not found')
     if request.method == 'GET':
-        if resp is None:
-            abort(404, 'Not found')
-
         return jsonify(resp.to_dict())
 
     if request.method == 'DELETE':
-        if resp is None:
-            abort(404, 'Not found')
-        else:
-            storage.delete(resp)
-            storage.save()
-            del resp
+        resp.delete()
+        storage.save()
         return make_response(jsonify({}), 200)
 
     if request.method == 'PUT':
-        if resp is None:
-            abort(404, 'Not found')
         data = request.get_json()
         if data is None:
             abort(400, 'Not a JSON')
 
         for key, value in data.items():
-            if key not in ['id', 'created_at', 'updated_at']:
+            if key not in ['id', 'email', 'created_at', 'updated_at']:
                 setattr(resp, key, value)
-        # obj = resp(**data)
         resp.save()
         return make_response(jsonify(resp.to_dict()), 200)
